ea459d2a474a1bc1f18a5734871b57bf35dd1e64
Emerson Lima <emersondll@outlook.com>
2022-11-15 22:23:13 -0300

    Tag ajustado

--- src/main/java/io/github/emersondll/transactions/controller/TransactionsController.java
+++ src/main/java/io/github/emersondll/transactions/controller/TransactionsController.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class TransactionsController implements BaseController {
 
     private TransactionsService service;
 
-    @RequestMapping("/transactions")
+    @PostMapping("/transactions")
     public ResponseEntity<TransactionsResponse> createTransaction(@RequestBody TransactionsRequest request) throws Exception {
         log.info("Start Transactions Controller");
         TransactionsResponse response = service.createTransaction(request);
